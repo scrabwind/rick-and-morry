d95cab49c36e9fa41bb573c42ff0cba6ea9a2e08
Kamil Wiatrowski <scrabwind@gmail.com>
2022-01-04 20:00:48 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import TopBar from './components/TopBar.vue'
+const color = 'yellow'
+</script>
+
+<template>
+	<TopBar msg="Hello Vue 3 + TypeScript + Vite" color="" />
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+
+body {
+	font-family: 'Poppins', sans-serif;
+	font-size: 16px;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	margin: 2rem;
+}
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string; color: string }>()
+</script>
+
+<template>
+	<img alt="Rick and Morty logo" src="../assets/logo.svg" />
+</template>
+
+<style scoped lang="scss"></style>
