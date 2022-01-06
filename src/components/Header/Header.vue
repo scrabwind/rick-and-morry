5aac5a2e0faf861a624756d42a7a58805fc13348
Kamil Wiatrowski <scrabwind@gmail.com>
2022-01-06 20:04:54 +0100

    feat(): add SeachBar component, remove SearchBar styling for remake

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,32 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import SearchBar from './SearchBar.vue'
 
 // defineProps<{ msg: string; color: string }>()
 </script>
 
 <template>
 	<div class="header">
-		<img alt="Rick and Morty logo" src="../assets/logo.svg" />
-		<div class="search"></div>
+		<img alt="Rick and Morty logo" src="../../assets/logo.svg" />
+		<SearchBar />
 	</div>
 </template>
 
 <style scoped lang="scss">
-// img {
-// 	width: 50px;
-// 	height: 30px;
-// }
 .header {
 	display: flex;
 	padding: 2rem;
 	border-bottom: 1px solid #e5eaf4;
 	align-items: center;
-}
-.search {
-	width: 500px;
-	height: 50px;
-	border: 1px solid #a9b1bd;
-	border-radius: 12px;
-	margin-left: 5rem;
 }
 </style>
---
+++ src/components/Header/SearchBar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import InlineSvg from 'vue-inline-svg'
+import arrow from '../../assets/play_arrow_black_24dp.svg'
+import search from '../../assets/search_black_24dp.svg'
+
+// defineProps<{ msg: string; color: string }>()
+</script>
+
+<template>
+	<div>
+		<span>Search By</span>
+		<button class="button">
+			<div>Name</div>
+			<inline-svg :src="arrow" class="svg" />
+		</button>
+		<div>
+			<input class="input" />
+			<inline-svg :src="search" class="svg" />
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.button,
+.input {
+	border: transparent;
+	background-color: transparent;
+}
+.svg {
+	fill: #a9b1bd;
+}
+</style>
